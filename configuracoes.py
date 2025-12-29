e0227f9ad8ababd5d1e5d4b9a114bdf6260657c2
Zhenriquee <83546851+Zhenriquee@users.noreply.github.com>
2025-12-29 17:15:22 +0000

    modified:   __pycache__/configuracoes.cpython-312.pyc 	modified:   configuracoes.py

--- configuracoes.py
+++ configuracoes.py
@@ -3,12 +3,15 @@ from pathlib import Path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# 2. Define o caminho da pasta de dados
 DB_FOLDER = os.path.join(BASE_DIR, 'data')
-DB_NAME = 'dados_ans_paralela.db'
-base_dire = Path(__file__).resolve().parents[2]
 
-DATABASE_PATH = base_dire / 'queries' / 'filtros' / 'listar_todas_operadoras.sql' #'/workspaces/ANALISE_OPERADORAS/data/base_ans_paralela.db'#os.path.join(DB_FOLDER, DB_NAME)
+DB_NAME = 'base_ans_paralela.db' 
 
+# 4. Constrói o caminho absoluto correto para o banco
+DATABASE_PATH = os.path.join(DB_FOLDER, DB_NAME)
+
+# 5. Define o caminho das queries
 QUERIES_PATH = os.path.join(BASE_DIR, 'queries')
 
 if not os.path.exists(DB_FOLDER):
